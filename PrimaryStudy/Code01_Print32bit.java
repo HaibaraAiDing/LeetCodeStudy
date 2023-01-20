b7bdd4edd670804866dcddf81ccc3bd422ecb19b
HoneyRainer <1120208217@qq.com>
2023-01-20 22:14:46 +0800

    测试

--- PrimaryStudy/Code01_Print32bit.java
+++ PrimaryStudy/Code01_Print32bit.java
@@ -11,7 +11,7 @@ public class Code01_Print32bit {
         for (int i = 31; i >= 0; i--){
             System.out.print((num & (1 << i)) == 0 ? "0" : "1");//1<<31表示当前数字向左移31位
         }
-        System.out.println();
+        System.out.println();//换行
     }
 
 }
